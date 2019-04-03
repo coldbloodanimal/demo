f69e8a6422e2c5278dcdaf51294239fcc129c799
zhaowei <dang7758@126.com>
2019-04-03 17:43:42 +0800

    用户信息模块

---
+++ demo-mybatis/src/main/java/com/example/Users.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import java.util.Date;
+
+public class Users {
+    private String id;
+
+    private String username;
+
+    private String password;
+
+    private Date createDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+}
