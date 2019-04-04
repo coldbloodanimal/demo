218d2529f247de700eb5575f325d940886ac6775
zhaowei <dang7758@126.com>
2019-04-04 17:49:13 +0800

    新增加一些基础

---
+++ demo-alibaba-oss/src/test/java/com/example/demoalibabaoss/DemoAlibabaOssApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demoalibabaoss;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoAlibabaOssApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ demo-java-base/src/test/java/com/example/demojavabase/DemoJavaBaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demojavabase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoJavaBaseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
