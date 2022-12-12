45bb2242e3eda8d9efacdba48233ea7803088b31
monster <dang7758@126.com>
2022-12-12 11:38:57 +0800

    something i forget

--- demo-springboot/src/main/java/com/example/SpringbootApplication.java
+++ demo-springboot/src/main/java/com/example/SpringbootApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootApplication {
-
+	/**
+	 * test
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication springApplication=new SpringApplication(SpringbootApplication.class);
 		springApplication.setBeanNameGenerator(new MyAnnotationBeanNameGenerator());
